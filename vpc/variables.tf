625640f00830315cb9a7703ac2911e2bc0af0881
jdhalleine-test <jdhalleine1@gmail.com>
2023-04-09 16:27:10 +0200

    update

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -10,4 +10,4 @@ variable "public_subnet_az2_cidr" {}
 variable "private_app_subnet_az1_cidr" {}
 variable "private_app_subnet_az2_cidr" {}
 variable "private_data_subnet_az1_cidr" {}
-variable "private_data_ subnet_az2_cidr" {}
+variable "private_data_subnet_az2_cidr" {}
