33f4fefcbff8df80401ade9b681b8fc0f713b2dd
jdhalleine <jdhalleine1@gmail.com>
2024-02-26 16:14:43 +0100

    update rds

--- rds/main.tf
+++ rds/main.tf
@@ -1,3 +1,14 @@
+# create database subnet group
+resource "aws_db_subnet_group" "database_subnet_group" {
+  name        = "${var.project_name}-${var.environment}-database-subnets"
+  subnet_ids  = [var.private_data_subnet_az1_id, var.private_data_subnet_az2_id]
+  description = "subnets for database instance"
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-database-subnets"
+  }
+}
+
 resource "aws_db_instance" "default" {
   allocated_storage    = 10
   db_name              = var.db_name
@@ -8,6 +19,12 @@ resource "aws_db_instance" "default" {
   password             = var.db_password
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
+  db_subnet_group_name = aws_db_subnet_group.database_subnet_group
+  multi_az             = false
+  vpc_security_group_ids = [var.database_security_group_id]
+  identifier             = "dev-rds-db"
+
+  availability_zone = var.availability_zone_1
 
     tags = {
     Name = "${var.project_name}-${var.environment}-rds"
