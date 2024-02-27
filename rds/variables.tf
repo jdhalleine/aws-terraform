281bbf46ff956b8688da807373d30fb8df460daf
jdhalleine <jdhalleine1@gmail.com>
2024-02-27 10:56:11 +0100

    add rds variables

--- rds/variables.tf
+++ rds/variables.tf
@@ -4,3 +4,7 @@ variable "environment" {}
 variable "db_username" {}
 variable "db_password" {}
 variable "db_name" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_data_subnet_az2_id" {}
+variable "database_security_group_id" {}
+variable "availability_zone_1" {}
